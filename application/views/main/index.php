6ab60aceefdd1e26f83d5ead88be112afcc14a16
Nadir <nadir008basalamah@gmail.com>
2019-07-30 11:22:08 +0700

    Adding new controllers and views

---
+++ application/views/main/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Klinik Apps</title>
+</head>
+<body>
+    <h1>Selamat Datang di KlinikApps</h1>
+    <p>Silahkan login dengan akun Anda</p>
+    <form action="#" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        <button type="submit">LOGIN</button>
+    </form>
+</body>
+</html>
