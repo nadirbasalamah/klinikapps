94034d3a095a8e1d9b868fdfaa7ffd2330c6cc4a
Nadir <nadir008basalamah@gmail.com>
2019-09-10 07:30:43 +0700

    Adding custom not found page

---
+++ application/controllers/Custom404.php
@@ -0,0 +1,14 @@
+<?php 
+class Custom404 extends CI_Controller 
+{
+ public function __construct() 
+ {
+    parent::__construct(); 
+ } 
+
+ public function index() 
+ { 
+    $this->output->set_status_header('404'); 
+    $this->load->view('main/not_found');
+ } 
+} 
